1f0bdd0a548f02fe16e41254cb43077fb8c74a52
JoséLuisB <joselui_b@hotmail.com>
2023-10-12 00:20:43 -0500

    carga de subdominios y respectivo archivo. v1.0

--- 1_subdominios.py
+++ 1_subdominios.py
@@ -1,20 +1,33 @@
-#importacion de libreriras
-import requests #consutas y peticiones a sitios web
-from os import path #comprobar existencia de rutas
-import argparse #poder agregar parametros a la hora de ejecucion
-import sys #funcionalidades adicionales
+# Importación de librerías
+import requests  # Para realizar consultas y peticiones a sitios web
+from os import path  # Para comprobar la existencia de rutas de archivos
+import argparse  # Para agregar argumentos a la hora de la ejecución del programa
+import sys  # Para funcionalidades adicionales del sistema
 
-#creamos una variable
-parser = argparse.ArgumentParser()
-#agregamos un argumento
-parser.add_argument("-t", "--target", help="Indica el dominio victima")
-#pasar el argumento a la variable parser
-parser = parser.parse_args()
+# Crear un analizador de argumentos con una descripción
+parser = argparse.ArgumentParser(description="Encuentra subdominios en un dominio víctima.")
+# Agregar un argumento requerido para el dominio objetivo
+parser.add_argument("-t", "--target", required=True, help="Dominio víctima")
+# Analizar los argumentos proporcionados por el usuario
+args = parser.parse_args()
 
 def main():
-    pass
-
-
+    if path.exists("1_subdominios.txt"):
+        # Abrir y leer el archivo "1_subdominios.txt" con subdominios separados por saltos de línea
+        wordlist = open("1_subdominios.txt", "r").read().split("\n")
+        
+        for i in wordlist:
+            for protocol in ["http://", "https://"]:
+                # Construir una URL combinando el protocolo, el subdominio y el dominio objetivo
+                url = f"{protocol}{i}.{args.target}"
+                try:
+                    # Realizar una solicitud web y obtener la respuesta
+                    response = requests.get(url)
+                    response.raise_for_status()  # Lanzar una excepción si la respuesta no es exitosa
+                    print(f"( * ) Subdominio encontrado: {url} ( * )")
+                except (requests.exceptions.RequestException, requests.ConnectionError):
+                    # Ignorar errores de conexión y solicitudes no exitosas
+                    pass
 
 if __name__ == "__main__":
     try:
