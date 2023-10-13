bbfa8db8013ae7280c43ea7d9631f0eb350880d6
JoséLuisB <joselui_b@hotmail.com>
2023-10-12 23:55:21 -0500

    finalizacion 2_wad

--- 2_wad/wad.py
+++ 2_wad/wad.py
@@ -1,25 +1,36 @@
-#Importamos librerias
 import subprocess
-import sys
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--target", help="Indica la URL ")
-parser = parser.parse_args()
-
 def main():
-    if parser.target:
-        subprocess.call("wad -u"+parser.target+"> tecnologias.txt", shell=True)
-        tecnologias = open("tecnologias.txt", "r")
-        tecnologias = tecnologias.read()
-        tecnologias = tecnologias.split("[")
-        tecnologias = tecnologias[1].split("]")
+    # Configuración de argumentos de línea de comandos
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", help="Indica la URL")
+    args = parser.parse_args()
+
+    if args.target:
+        try:
+            # Ejecuta el comando "wad" para obtener información sobre las tecnologías en la URL objetivo
+            result = subprocess.check_output(["wad", "-u", args.target], text=True)
+        except subprocess.CalledProcessError:
+            print("Error al ejecutar el comando 'wad'")
+            return
+
+        # Busca la información de tecnologías entre corchetes en la salida del comando
+        start_index = result.find("[")
+        end_index = result.find("]", start_index)
+
+        if start_index != -1 and end_index != -1:
+            tecnologias = result[start_index + 1:end_index]
+            
+            # Abre un archivo para escritura y escribe las tecnologías encontradas
+            with open("tecnologias.txt", "w") as file:
+                file.write(tecnologias)
+            
+            print("Tecnologías encontradas:", tecnologias)
+        else:
+            print("No se encontraron tecnologías en la URL proporcionada.")
     else:
         print("Ingresa una URL")
 
-
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt():
-        exit()
+    main()
